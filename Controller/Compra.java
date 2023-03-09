84586cb21031087d7847cb61ff822d64c60ae8b9
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-03-08 23:20:25 -0300

    Iniciando logica de negocio

---
+++ Controller/Compra.java
@@ -0,0 +1,19 @@
+package Controller;
+
+public class Compra {
+    public static void comprarProducto(Tienda tienda, Producto producto, int cantidad) {
+
+    }
+    private static void actualizarSaldo(Tienda tienda, Producto producto, int cantidad) {
+
+    }
+
+    private static void verificarSaldo(Tienda tienda, Producto producto, int cantidad) {
+
+    }
+
+    private static void agregarProducto(Tienda tienda, Producto producto, int cantidad) {
+        tienda.getProductos().push(producto);
+    }
+
+}
---
+++ Controller/Stock.java
@@ -0,0 +1,10 @@
+package Controller;
+
+public class Stock {
+    public static void actualizarStock(Tienda tienda, Producto producto, int cantidad) {
+
+    }
+    public static void verificarStock(Tienda tienda, Producto producto, int cantidad) {
+
+    }
+}
